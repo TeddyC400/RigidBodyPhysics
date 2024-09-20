8ac1e61647d1408624fbe0d6096e343935a3a527
C400 <tblaze28@gmail.com>
2024-09-19 23:54:04 -0400

    Remove unused java classes

--- src/main/java/io/github/tblaze/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.tblaze;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/main/java/io/github/tblaze/WorldBlock.java
+++
@@ -1,9 +0,0 @@
-package io.github.tblaze;
-
-import net.minestom.server.coordinate.Point;
-import net.minestom.server.instance.block.Block;
-
-public record WorldBlock(Point position, Block block) {
-
-
-}
